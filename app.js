eefda0e6bae2eb43320cac013f1eb2444b7cfd0c
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2023-06-27 15:28:46 +0900

    localStorage에 저장된 username이 있냐 없냐에 따라 다르게 실행되기 만듦.

--- app.js
+++ app.js
@@ -3,14 +3,28 @@ const loginInput = document.querySelector('#login-form input');
 const greeting = document.querySelector('#greeting');
 
 const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'username';
 
 function onLoginSubmit(event) {
-  event.preventDefault();
+  event.preventDefault(); //새로고침 막기
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem('username', username); //유저 이름 저장하기
-  greeting.innerText = `Hello ${username}!`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem(USERNAME_KEY, username); //localStorage에 username 추가
+  paintGreetings(username);
 }
 
-loginForm.addEventListener('submit', onLoginSubmit);
+function paintGreetings(username) {
+  greeting.innerText = `Hello ${username}!`; //텍스트 추가하고
+  greeting.classList.remove(HIDDEN_CLASSNAME); //greeting을 보여주기
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+  //저장된 username이 없으면
+  loginForm.classList.remove(HIDDEN_CLASSNAME); // form을 보여주고
+  loginForm.addEventListener('submit', onLoginSubmit); //이벤트 실행
+} else {
+  //저장된 username이 있으면
+  paintGreetings(savedUsername);
+}
