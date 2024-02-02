e9a9b9bb6f09eea414cb1d9aaba5058802a4d606
bomi0320 <84120820+bomi0320@users.noreply.github.com>
2024-02-02 22:23:17 +0900

    localStorage에 todo list 저장

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,12 @@ const toDoForm = document.getElementById('todo-form');
 const toDoInput = toDoForm.querySelector('input');
 const toDoList = document.getElementById('todo-list');
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem('todos', toDos);
+}
+
 function deleteToDo(event) {
   const li = event.target.parentElement; //부모인 li 찾기
   li.remove(); //li 삭제!
@@ -23,7 +29,9 @@ function handleToDoSubmit(event) {
   event.preventDefault(); //새로고침 막기
   const newTodo = toDoInput.value; //input에 적힌 text 저장하기
   toDoInput.value = ''; //제출한 후 input 비우기
+  toDos.push(newTodo); //toDos array에 newTodo 넣기
   paintToDo(newTodo);
+  saveToDos();
 }
 
 toDoForm.addEventListener('submit', handleToDoSubmit);
